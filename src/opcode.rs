d3aaff906d5fd090ac07c9b744488d0683ae7e79
Kentaro Okuda <lonesometraveler@mac.com>
2023-11-30 07:12:14 -0500

    Refactor CPU

--- src/opcode.rs
+++ src/opcode.rs
@@ -88,6 +88,29 @@ pub enum AddressingMode {
     Relative,
 }
 
+impl AddressingMode {
+    pub fn n_bytes(&self) -> usize {
+        match *self {
+            AddressingMode::Implied | AddressingMode::Accumulator => 1,
+
+            AddressingMode::Immediate
+            | AddressingMode::ZeroPageIndexed
+            | AddressingMode::Relative
+            | AddressingMode::ZeroPageX
+            | AddressingMode::ZeroPageY
+            | AddressingMode::IndexedIndirect
+            | AddressingMode::IndirectIndexed => 2,
+
+            AddressingMode::Absolute
+            | AddressingMode::AbsoluteX
+            | AddressingMode::AbsoluteY
+            | AddressingMode::Indirect => 3,
+
+            _ => panic!("Bad addressing mode {:?}", *self),
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 pub(crate) struct Opcode(
     pub(crate) Instruction,
